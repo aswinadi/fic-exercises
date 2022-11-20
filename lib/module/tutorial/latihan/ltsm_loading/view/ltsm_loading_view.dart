f2792f9239224b7a4ce647bd9c7794e8d09e44ab
Aswin Adi Saputra <aswin.adisaputra@gmail.com>
2022-11-20 19:24:35 +0700

    loading exercise

--- lib/module/tutorial/latihan/ltsm_loading/view/ltsm_loading_view.dart
+++ lib/module/tutorial/latihan/ltsm_loading/view/ltsm_loading_view.dart
@@ -17,7 +17,7 @@ class LtsmLoadingView extends StatefulWidget {
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [
+            children: [
               /*
               ? Buat variabel loading di dalam State/Controller
               ! bool loading = false;
@@ -33,6 +33,18 @@ class LtsmLoadingView extends StatefulWidget {
               */
                 
               */
+              if (controller.loading) const CircularProgressIndicator(),
+              ElevatedButton.icon(
+                onPressed: () {
+                  controller.loading = !controller.loading;
+                  controller.setState(() {});
+                },
+                icon: const Icon(
+                  Icons.developer_board,
+                  size: 24.0,
+                ),
+                label: const Text("loading = true"),
+              ),
             ],
           ),
         ),
